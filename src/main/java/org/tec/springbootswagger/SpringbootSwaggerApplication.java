4195dc7ce6a2e6dd8e42c1fb0ca818960c26545f
tim-oe <tim@outboundengine.com>
2019-01-18 13:58:38 -0600

    enabled monitoring access default security

--- src/main/java/org/tec/springbootswagger/SpringbootSwaggerApplication.java
+++ src/main/java/org/tec/springbootswagger/SpringbootSwaggerApplication.java
@@ -2,17 +2,16 @@ package org.tec.springbootswagger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-/*
 @PropertySources({
         @PropertySource(value="file:${user.home}/.application.properties", ignoreResourceNotFound = true)
 })
-*/
 public class SpringbootSwaggerApplication {
-
     /**
      * spring boot application entry point
      * @param args command line args if any
