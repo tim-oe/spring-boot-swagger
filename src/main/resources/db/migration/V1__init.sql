99778eb9b488797453dbc5029e4fa58e736ccc13
tim-oe <tim@outboundengine.com>
2019-01-18 08:37:30 -0600

    added flyway for db population

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `people` (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `email` varchar(100) NOT NULL COMMENT 'email address',
+  `first_name` varchar(30) DEFAULT NULL COMMENT 'first name',
+  `last_name` varchar(50) DEFAULT NULL COMMENT 'last name',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email_` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
